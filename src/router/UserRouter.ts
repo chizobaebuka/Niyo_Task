67613f10cb3f12c63bf321fb82114b76caa0c022
martins <chizobaebuka1@gmail.com>
2024-05-16 00:24:30 +0100

    added logout User and delete User functionalities;

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -1,8 +1,6 @@
 import userController from "../controller/userController";
 import { createUserSchema, loginUserSchema } from "../schema/UserSchema";
 import validate from "../utils/validate";
-// import validate from "../helpers/validate";
-// import { createUserSchema, loginUserSchema, updateUserSchema } from "../schema/userSchema";
 import BaseRoutes from "./base/BaseRouter";
 
 
@@ -11,9 +9,11 @@ class UserRoutes extends BaseRoutes {
     public routes(): void {
         this.router.post("/", validate(createUserSchema), userController.createUser),
         this.router.post('/login', validate(loginUserSchema), userController.loginUser),
+        this.router.post('/logout', userController.logoutUser),
         this.router.get('/', userController.findAllUsers),
         this.router.get('/:id', userController.findUserById),
-        this.router.patch('/:id', userController.updateUser)
+        this.router.patch('/:id', userController.updateUser),
+        this.router.delete('/:id', userController.deleteUser)
     }
 }
 
